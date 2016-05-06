908a5d403a3056d267c9f6e7f6a18edc48b61e27
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2016-05-06 15:08:40 -0700

    Complete react components mini sprint

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 // TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    }
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)}>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+let GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(groceryItem =>
+      <GroceryListItem groceryItem={groceryItem} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList groceryItems={['Apples', 'Oranges']}/>, document.getElementById("app"));
